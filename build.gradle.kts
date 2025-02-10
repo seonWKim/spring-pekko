12db05bc6e93880312ac653f2915c7c88abe9d76
김선우 <seonwoo960000@toss.im>
2025-02-10 11:40:37 +0900

    Add actor dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,14 @@ subprojects {
 	}
 
 	dependencies {
+		implementation("org.apache.pekko:pekko-actor-typed_3:1.1.3")
+		implementation("org.apache.pekko:pekko-cluster-typed_3:1.1.3")
+		implementation("org.apache.pekko:pekko-cluster-sharding-typed_3:1.1.3")
+
 		implementation("org.springframework.boot:spring-boot-starter")
+
 		testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	}
 
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
+	implementation("org.apache.pekko:pekko-actor-typed_3")
+	implementation("org.apache.pekko:pekko-cluster-typed_3")
+	implementation("org.apache.pekko:pekko-cluster-sharding-typed_3")
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")

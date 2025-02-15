65e5f83bf60456c93b80f35c07d8b5713de71e16
김선우 <seonwoo960000@toss.im>
2025-02-15 14:51:04 +0900

    Set up integration test for cluster mode

--- core/src/test/java/org/github/seonwkim/integration/ClusterTest.java
+++ core/src/test/java/org/github/seonwkim/integration/ClusterTest.java
@@ -2,12 +2,16 @@ package org.github.seonwkim.integration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.github.seonwkim.core.PekkoAutoConfiguration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.TestPropertySource;
 
 public class ClusterTest {
@@ -16,54 +20,18 @@ public class ClusterTest {
     private static ConfigurableApplicationContext context2;
     private static ConfigurableApplicationContext context3;
 
-    @Configuration
-    @TestPropertySource(
-            properties = {
-                    "server.port=16001",
-                    "pekko.name=cluster1",
-                    "pekko.actor.provider=cluster",
-                    "pekko.actor.allow-java-serialization=on",
-                    "pekko.actor.warn-about-java-serializer-usage=off",
-                    "pekko.remote.artery.canonical.hostname=127.0.0.1",
-                    "pekko.remote.artery.canonical.port=62552",
-                    "pekko.cluster.name=clusterName1",
-                    "pekko.cluster.seed-nodes=pekko://clusterName1@127.0.0.1:62552,pekko://clusterName1@127.0.0.1:62553,pekko://clusterName1@127.0.0.1:62554",
-                    "pekko.cluster.downing-provider-class=org.apache.pekko.cluster.sbr.SplitBrainResolverProvider"
-            })
+    @PropertySource("classpath:application1.properties")
+    @SpringBootApplication(scanBasePackages = "org.github.seonwkim.core")
     static class Application1Config {
     }
 
-    @Configuration
-    @TestPropertySource(
-            properties = {
-                    "server.port=16002",
-                    "pekko.name=cluster2",
-                    "pekko.actor.provider=cluster",
-                    "pekko.actor.allow-java-serialization=on",
-                    "pekko.actor.warn-about-java-serializer-usage=off",
-                    "pekko.remote.artery.canonical.hostname=127.0.0.1",
-                    "pekko.remote.artery.canonical.port=62553",
-                    "pekko.cluster.name=clusterName2",
-                    "pekko.cluster.seed-nodes=pekko://clusterName2@127.0.0.1:62552,pekko://clusterName2@127.0.0.1:62553,pekko://clusterName2@127.0.0.1:62554",
-                    "pekko.cluster.downing-provider-class=org.apache.pekko.cluster.sbr.SplitBrainResolverProvider"
-            })
+    @PropertySource("classpath:application2.properties")
+    @SpringBootApplication(scanBasePackages = "org.github.seonwkim.core")
     static class Application2Config {
     }
 
-    @Configuration
-    @TestPropertySource(
-            properties = {
-                    "server.port=16003",
-                    "pekko.name=cluster3",
-                    "pekko.actor.provider=cluster",
-                    "pekko.actor.allow-java-serialization=on",
-                    "pekko.actor.warn-about-java-serializer-usage=off",
-                    "pekko.remote.artery.canonical.hostname=127.0.0.1",
-                    "pekko.remote.artery.canonical.port=62554",
-                    "pekko.cluster.name=clusterName3",
-                    "pekko.cluster.seed-nodes=pekko://clusterName3@127.0.0.1:62552,pekko://clusterName3@127.0.0.1:62553,pekko://clusterName3@127.0.0.1:62554",
-                    "pekko.cluster.downing-provider-class=org.apache.pekko.cluster.sbr.SplitBrainResolverProvider"
-            })
+    @PropertySource("classpath:application3.properties")
+    @SpringBootApplication(scanBasePackages = "org.github.seonwkim.core")
     static class Application3Config {
     }
 
@@ -80,9 +48,9 @@ public class ClusterTest {
         String pekkoName2 = context2.getEnvironment().getProperty("pekko.name");
         String pekkoName3 = context3.getEnvironment().getProperty("pekko.name");
 
-        assertEquals("cluster1", pekkoName1);
-        assertEquals("cluster2", pekkoName2);
-        assertEquals("cluster3", pekkoName3);
+        assertEquals("cluster-node-1", pekkoName1);
+        assertEquals("cluster-node-2", pekkoName2);
+        assertEquals("cluster-node-3", pekkoName3);
     }
 
     @AfterAll
